fb41139790dbd8a0ce0c29cd5923550468586483
abrahambeltran <abraham.beltran.2001@gmail.com>
2022-11-07 10:44:45 -0500

    started working on some frontend to backend connections

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+      fetch("/members").then(
+        res => res.json()
+      ).then(
+        data => {
+            setData(data)
+            console.log(data)
+        }
+      )
+  }, [])
+
+  return (
+    <div>
+      App
+    </div>
+  )
 }
 
-export default App;
+export default App
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,68 +1,10 @@
-'''
-import http.client
+from flask import Flask
 
-conn = http.client.HTTPSConnection("hacker-news.firebaseio.com")
+app = Flask(__name__)
 
-payload = "{}"
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "member2"]}
 
-conn.request("GET", "/v0/topstories.json?print=pretty", payload)
-
-res = conn.getresponse()
-data = res.read()
-
-d_data = data.decode("utf-8")
-
-print(d_data[0])
-
-
-import requests
-
-url = "https://hacker-news.firebaseio.com/v0/showstories.json"
-
-payload = "{}"
-response = requests.request("GET", url, data=payload)
-
-print(response.text)
-
-
-https://hacker-news.firebaseio.com/v0/item/33405997.json?print=pretty
-'''
-from operator import itemgetter
-import requests
-
-#Make an API call and store the response
-url='https://hacker-news.firebaseio.com/v0/showstories.json'
-r=requests.get(url)
-print(f"Status Code:{r.status_code}")
-
-#Process the information about each submission
-submission_ids=r.json()
-submission_dicts= {}
-i = 0
-for submission_id in submission_ids[:30]:
-    #Make a seperate api call for each id
-    url1=f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json?print=pretty"
-    r1=requests.get(url1)
-    response_dict=r1.json()
-    print(response_dict['title'])
-    
-    
-    submission_dict[i]={
-            'title':response_dict['title'],
-            'hn_link':f"http://news.ycombinator.com/item?id={submission_id}",
-            'comments':response_dict['descendants']
-        }
-    i += 1
-
-print(submission_dict)
-
-
-
-'''
-    submission_dicts.append(submission_dict)
-submission_dicts=sorted(submission_dicts,key=itemgetter('comments'),reverse=True)
-for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dicts.get('title')}")
-    print(f"\nDiscuission link: {submission_dicts.get('hn_link')}")
-
-'''
+if __name__ == "__main__":
+    app.run(debug=True)
